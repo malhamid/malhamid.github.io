071ee681b240251d1b7e98171997befb21530b84
malhamid <moh.alhamid@gmail.com>
2019-04-06 13:11:29 -0400

    Adding a testing post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Three years of problems with an iMac G5",
+        "excerpt":"Welcome I recently spent some time improving the typography of this blog. Here are the 3 things that had the biggest impact on improving my site’s readability. 1. Steal typography from sites that you like Look at the sites you frequent and take note of their readability. Inspect their CSS...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/HelloWorld/",
+        "teaser":null}]
